2f47b26501291af746b8b6a896c712b71a36066c
Alvaro Garcia <derecerca@gmail.com>
2018-09-01 23:59:14 +0200

    Acceptance green

--- src/main/kotlin/com/example/kata/lift/Lift.kt
+++ src/main/kotlin/com/example/kata/lift/Lift.kt
@@ -2,8 +2,17 @@ package com.example.kata.lift
 
 class Lift(val at: Int, val goingTo: Direction) {
 
+    private val queuedCalls = mutableListOf<Call>()
+
     fun calledFrom(floor: Int, direction: Direction): Lift {
-        return Lift(floor, direction)
+        if (this.goingTo == Direction.STOPPED || direction == this.goingTo) {
+            return Lift(floor, direction)
+        } else {
+            queuedCalls.add(Call(floor, direction))
+            return this
+        }
     }
+
+    data class Call(val floor: Int, val direction: Direction)
 
 }
