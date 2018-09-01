64617a5c925f939131e9d96bcbc63f459bb510c2
Alvaro Garcia <derecerca@gmail.com>
2018-09-01 23:59:57 +0200

    Refactor: extract explaining method

--- src/main/kotlin/com/example/kata/lift/Lift.kt
+++ src/main/kotlin/com/example/kata/lift/Lift.kt
@@ -5,13 +5,16 @@ class Lift(val at: Int, val goingTo: Direction) {
     private val queuedCalls = mutableListOf<Call>()
 
     fun calledFrom(floor: Int, direction: Direction): Lift {
-        return if (this.goingTo == Direction.STOPPED || direction == this.goingTo) {
+        return if (goingTowards(direction)) {
             Lift(floor, direction)
         } else {
             queuedCalls.add(Call(floor, direction))
             this
         }
     }
+
+    private fun goingTowards(direction: Direction) =
+            this.goingTo == Direction.STOPPED || direction == this.goingTo
 
     data class Call(val floor: Int, val direction: Direction)
 
