a66ebc57ea25964ce7117a54bb89f1b190443994
Alvaro Garcia <derecerca@gmail.com>
2018-09-01 23:59:27 +0200

    Refactor: move return out of if

--- src/main/kotlin/com/example/kata/lift/Lift.kt
+++ src/main/kotlin/com/example/kata/lift/Lift.kt
@@ -5,11 +5,11 @@ class Lift(val at: Int, val goingTo: Direction) {
     private val queuedCalls = mutableListOf<Call>()
 
     fun calledFrom(floor: Int, direction: Direction): Lift {
-        if (this.goingTo == Direction.STOPPED || direction == this.goingTo) {
-            return Lift(floor, direction)
+        return if (this.goingTo == Direction.STOPPED || direction == this.goingTo) {
+            Lift(floor, direction)
         } else {
             queuedCalls.add(Call(floor, direction))
-            return this
+            this
         }
     }
 
